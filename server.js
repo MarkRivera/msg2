0d62a5d233aaa45e1019c7e836fa36999a9ed687
Mark Rivera <Mrivera1991@gmail.com>
2019-08-07 22:08:46 -0400

    next commit

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const server = express();
 const connectDB = require("./config/db");
+const path = require("path")
 
 connectDB();
 
 server.use(express.json({ extended: false }));
+if (process.env.NODE_ENV === "production") {
+	server.use(express.static("client/build"));
 
-server.get("/", (req, res) => {
-	res.send("API is listening");
-});
+	server.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 // server.use("/api/users", require("./routes/api/users"));
 
