b5d30f286a3ba37746fffeaa3cbba88ae19b0f98
Mark Rivera <Mrivera1991@gmail.com>
2019-08-09 10:54:34 -0400

    deleted build folder

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.use(express.json({ extended: false }));
 if (process.env.NODE_ENV === "production") {
 	server.use(express.static(path.join(__dirname, "client", "build")));
 
-	server.get("/", (req, res) => {
+	server.get("/*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
